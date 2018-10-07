501153a05edd1e45fc644508fc563daf3133317c
raineldn <raineldn@gmail.com>
2018-10-07 13:02:19 +0100

    added css, html and js files

---
+++ ColorGame.js
@@ -0,0 +1,57 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)",
+]
+
+var colors = generateRandomColors(6);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+		} else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	})
+}
+
+function changeColors(color){
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++){
+	//change each color to match given color
+	squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length)
+	return colors[random];
+}
+
+
+
+
+
+
+
+
+
